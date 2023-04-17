551e7a0f9cacfccff7c69a36e176878f42988089
reihaneh <reihaneh763@gmail.com>
2023-04-18 01:20:05 +0430

    add UserDto class

--- src/main/java/com/challenge/socialnetworkv2/data/dto/UserDto.java
+++ src/main/java/com/challenge/socialnetworkv2/data/dto/UserDto.java
@@ -1,4 +1,30 @@
 package com.challenge.socialnetworkv2.data.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
+    private Long id;
+    @NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "invalid Email!")
+    private String email;
+    @NotEmpty(message = "Password should not be empty")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–{}:;',?/*~$^+=<>]).{8,20}$",
+            message = "invalid password!")
+    private String password;
+
+    byte[] personalPhoto;
 }
