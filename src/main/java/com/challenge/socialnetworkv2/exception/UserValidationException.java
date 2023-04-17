4e75538d34810fc6b447faaa7defcde4e6277b63
reihaneh <reihaneh763@gmail.com>
2023-04-18 01:15:48 +0430

    add UserValidationException class

--- src/main/java/com/challenge/socialnetworkv2/exception/UserValidationException.java
+++ src/main/java/com/challenge/socialnetworkv2/exception/UserValidationException.java
@@ -1,2 +1,7 @@
-package com.challenge.socialnetworkv2.exception;public class UserValidationException {
+package com.challenge.socialnetworkv2.exception;
+
+public class UserValidationException extends RuntimeException {
+    public UserValidationException(String message) {
+        super(message);
+    }
 }
