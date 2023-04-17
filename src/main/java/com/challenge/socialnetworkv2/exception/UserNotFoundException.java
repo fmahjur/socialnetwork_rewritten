c62197a42f1a3717f2e26a38c414e121e39c6c55
reihaneh <reihaneh763@gmail.com>
2023-04-18 01:15:28 +0430

    add UserNotFoundException class

--- src/main/java/com/challenge/socialnetworkv2/exception/UserNotFoundException.java
+++ src/main/java/com/challenge/socialnetworkv2/exception/UserNotFoundException.java
@@ -1,4 +1,7 @@
 package com.challenge.socialnetworkv2.exception;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
