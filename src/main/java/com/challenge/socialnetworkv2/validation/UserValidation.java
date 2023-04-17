f5c0ec66609848fd77b615623b9c6bd5ff7e9dbb
reihaneh <reihaneh763@gmail.com>
2023-04-18 01:17:37 +0430

    add UserValidation class

--- src/main/java/com/challenge/socialnetworkv2/validation/UserValidation.java
+++ src/main/java/com/challenge/socialnetworkv2/validation/UserValidation.java
@@ -1,4 +1,17 @@
 package com.challenge.socialnetworkv2.validation;
 
+import com.challenge.socialnetworkv2.data.repository.UsersRepository;
+import com.challenge.socialnetworkv2.exception.UserValidationException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class UserValidation {
+    private static UsersRepository usersRepository;
+
+    public static void isExistUser(String email) {
+        if (usersRepository.existsByEmail(email))
+            throw new UserValidationException("this Email is already Exist!");
+    }
 }
