bda2a1d3bb6cc52729533431daa6befd5364135a
reihaneh <reihaneh763@gmail.com>
2023-04-18 01:22:46 +0430

    add UsersServiceImpl class

--- src/main/java/com/challenge/socialnetworkv2/service/impl/UsersServiceImpl.java
+++ src/main/java/com/challenge/socialnetworkv2/service/impl/UsersServiceImpl.java
@@ -8,7 +8,7 @@ import com.challenge.socialnetworkv2.exception.UserNotFoundException;
 import com.challenge.socialnetworkv2.service.FriendshipService;
 import com.challenge.socialnetworkv2.service.UsersService;
 import com.challenge.socialnetworkv2.validation.PictureValidator;
-import com.challenge.socialnetworkv2.validation.UserValidation;
+import com.challenge.socialnetworkv2.validation.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,7 +16,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,7 +41,7 @@ public class UsersServiceImpl implements UserDetailsService, UsersService {
 
     @Override
     public void registerUser(UserDto userDto) {
-        UserValidation.isExistUser(userDto.getEmail());
+        UserValidator.isExistUser(userDto.getEmail());
         PictureValidator.isValidImageSize(userDto.getPersonalPhoto());
         Users user = new Users();
         user.setFirstname(userDto.getFirstName());
