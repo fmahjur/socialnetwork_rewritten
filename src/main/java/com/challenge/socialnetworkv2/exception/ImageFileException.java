eddfd855c65a3d8bfe6edf3878044b286b79229f
reihaneh <reihaneh763@gmail.com>
2023-04-18 01:14:41 +0430

    add ImageFileException class

--- src/main/java/com/challenge/socialnetworkv2/exception/ImageFileException.java
+++ src/main/java/com/challenge/socialnetworkv2/exception/ImageFileException.java
@@ -1,2 +1,7 @@
-package com.challenge.socialnetworkv2.exception;public class ImageFileException {
+package com.challenge.socialnetworkv2.exception;
+
+public class ImageFileException extends RuntimeException {
+    public ImageFileException(String message) {
+        super(message);
+    }
 }
